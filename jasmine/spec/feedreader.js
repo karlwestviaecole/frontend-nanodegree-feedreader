6bf17701c11286876a8c99151f4c127e11206740
Karl West <karl.west@viaecole.se>
2018-10-16 11:37:37 +0200

    added test suite The menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,10 +1,5 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
 $(function () {
- 
+
     describe('RSS Feeds', function () {
 
         it('are defined', function () {
@@ -29,20 +24,22 @@ $(function () {
         });
     });
 
+    describe('The menu', function () {
+        
+        /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the
+         * hiding/showing of the menu element.
+         */
 
-    /* TODO: Write a new test suite named "The menu" */
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+    });
 
-    /* TODO: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
-     */
 
-    /* TODO: Write a test that ensures the menu changes
-     * visibility when the menu icon is clicked. This test
-     * should have two expectations: does the menu display when
-     * clicked and does it hide when clicked again.
-     */
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
