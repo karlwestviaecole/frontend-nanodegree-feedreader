2304f67314f82cea8646c56186885dcf51bbfb07
Karl West <karl.west@viaecole.se>
2018-10-16 12:15:40 +0200

    test initial entries are not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -40,19 +40,24 @@ $(function () {
 
     describe('Initial Entries', function () {
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        beforeEach(function (done) {
+            loadFeed(0, function () {
+                done();
+            });
+        });
 
-        /* TODO: Write a new test suite named "New Feed Selection" */
+        it('should not be empty', function (done) {
+            expect($('.feed').find('.entry').length).toBeGreaterThan(0);
+            done();
+        });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
     });
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
 
 }());
